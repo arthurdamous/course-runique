36fd319396f2806f2608996aa9947ded6680ad45
Arthur Damous <arthur.damous@gmail.com>
2024-10-23 00:29:14 -0300

    Added files'

--- run/presentation/src/main/java/com/plcoding/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/plcoding/run/presentation/active_run/ActiveRunViewModel.kt
@@ -4,10 +4,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.run.domain.RunningTracker
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import timber.log.Timber
 
-class ActiveRunViewModel : ViewModel() {
+class ActiveRunViewModel(
+    private val runningTracker: RunningTracker,
+) : ViewModel() {
 
     var state by mutableStateOf(ActiveRunState())
         private set
@@ -15,7 +23,25 @@ class ActiveRunViewModel : ViewModel() {
     private val eventChannel = Channel<ActiveRunEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    private val _hasLocationPermission = mutableStateOf(false)
+    private val _hasLocationPermission = MutableStateFlow(false)
+
+    init {
+        _hasLocationPermission
+            .onEach { hasPermission ->
+                if (hasPermission) {
+                    runningTracker.startObservingLocation()
+                } else {
+                    runningTracker.stopObservingLocation()
+                }
+            }
+            .launchIn(viewModelScope)
+
+        runningTracker.currentLocation
+            .onEach { location ->
+                Timber.d("New location: $location")
+            }
+            .launchIn(viewModelScope)
+    }
 
     fun onAction(action: ActiveRunAction) {
         when (action) {
@@ -23,17 +49,21 @@ class ActiveRunViewModel : ViewModel() {
             ActiveRunAction.OnFinishRunClick -> {
 
             }
+
             ActiveRunAction.OnResumeRunClick -> {
             }
+
             ActiveRunAction.OnToggleRunClick -> {
 
             }
+
             is ActiveRunAction.SubmitLocationPermissionInfo -> {
                 _hasLocationPermission.value = action.acceptedLocationPermission
                 state = state.copy(
                     showLocationRationale = action.showLocationRationale
                 )
             }
+
             is ActiveRunAction.SubmitNotificationPermissionInfo -> {
                 state = state.copy(
                     showNotificationRationale = action.showNotificationPermissionRationale
