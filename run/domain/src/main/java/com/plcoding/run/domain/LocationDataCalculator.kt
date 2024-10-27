2f6f49c68de248c403c2a6d7c7aad4f08ba2f747
Arthur Damous <arthur.damous@gmail.com>
2024-10-27 01:38:03 -0300

    Added files

---
+++ run/domain/src/main/java/com/plcoding/run/domain/LocationDataCalculator.kt
@@ -0,0 +1,19 @@
+package com.plcoding.run.domain
+
+import com.plcoding.core.domain.location.LocationTimestamp
+import kotlin.math.roundToInt
+
+object LocationDataCalculator {
+
+    fun getTotalDistanceMeters(locations: List<List<LocationTimestamp>>): Int {
+        return locations
+            .sumOf { timeStampsPerLine ->
+                timeStampsPerLine.zipWithNext { location1, location2 ->
+                    location1.location.location.distanceTo(location2.location.location)
+
+                }.sum().roundToInt()
+            }
+    }
+
+
+}
